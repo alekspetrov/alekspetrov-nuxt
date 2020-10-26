26052c16765d9d48a3752051de42d762abd1c043
Aleks Petrov <hello@alekspetrov.com>
2020-10-26 16:54:32 +0100

    Update linter

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'max-nesting-depth': 3,
+    'selector-max-compound-selectors': 4,
+    'no-descending-specificity': null,
+    'selector-no-qualifying-type': null,
+  },
 }
